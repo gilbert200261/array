bfd27d1808be07df3f62eab5f486eec36ef18074
sarah hatfield <gilbert200261@yahoo.com>
2026-04-13 22:55:45 -0400

    first commit

---
+++ C# projects/In class/program.cs
@@ -0,0 +1,7 @@
+       
+       using System;
+       
+       Console.WriteLine("Hello!");
+       Console.Write("What is your name? ");
+       string name = Console.ReadLine();
+       Console.WriteLine("Nice to meet you, " + name + "!");
